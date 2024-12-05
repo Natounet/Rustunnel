cd7db01d06c8963f27ad33d4062e49b90c2b1a4e
Nathan CORNELOUP <naytan@protonmail.com>
2024-12-05 18:06:00 +0100

    Fix: Logique incorrecte dans split_data_into_label qui n'avait pas le bon nombre de bytes par label avant encodage + ajout de tests

--- rustunnel-client/src/utils.rs
+++ rustunnel-client/src/utils.rs
@@ -1,15 +1,15 @@
-use base32::{Alphabet, encode as base32_encode, decode as base32_decode};
+use base32::{decode as base32_decode, encode as base32_encode, Alphabet};
 use rand::Rng;
 
-/// Divise une séquence d’octets (bytes) en labels de taille maximale 63 caractères
-pub fn split_bytes_into_labels(bytes: &[u8]) -> Vec<Vec<u8>> {
+// 63 chars base32 encoded = 39 bytes
+pub fn split_data_into_label_chunks(bytes: &[u8]) -> Vec<Vec<u8>> {
     let mut labels = Vec::new();
     let mut current_label = Vec::new();
 
     for &byte in bytes {
         current_label.push(byte);
 
-        if current_label.len() == 63 {
+        if current_label.len() == 39 {
             labels.push(current_label);
             current_label = Vec::new();
         }
@@ -44,4 +44,27 @@ pub fn decode_base32(labels: Vec<String>) -> Vec<Vec<u8>> {
 // Fonction de génération d'un UUID u16 aléatoire
 pub fn generate_u16_uuid() -> u16 {
     rand::thread_rng().gen()
+}
+
+/* ====================== TESTS ====================== */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_base32_length_limit() {
+        let test_sizes: Vec<usize> = (0..100).map(|_| rand::random::<usize>() % 10000).collect();
+
+        for size in test_sizes {
+            let random_bytes: Vec<u8> = (0..size).map(|_| rand::random::<u8>()).collect();
+            let encoded = encode_base32(split_data_into_label_chunks(&random_bytes));
+            assert!(
+                encoded.iter().all(|label| label.len() <= 63),
+                "A label in {:?}  exceeds DNS limit of 63 chars (input size: {})",
+                encoded,
+                size
+            );
+        }
+    }
 }
